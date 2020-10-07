175a5bbb13854bba617d6638928b3edbcd3031c7
“daniel_penumala” <penumala.gmail.com>
2020-10-07 18:01:53 -0500

    Project and UI Setup

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 console.log("Script Connected");
+const newGame = document.querySelector('.New');
+const reset = document.querySelector('.Reset');
+const box = document.querySelectorAll('.cell');
+//const grid = box;
+
+
+console.log(box);
+const gameOn = true;
+
+
+
+reset.addEventListener('click',(event)=>{     
+    event.preventDefault()
+    console.log("Reset");
+    box.innerText="";
+   
+})
+
+
